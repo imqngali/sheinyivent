c0f16aa9110f909fb680a44738f19483302f9056
imqngali <imangaliisatai1@gmail.com>
2023-06-20 16:33:55 +0600

    form

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 AOS.init()
 
 $(document).ready(function() {
+
+	console.log("dad");
+     
+	$(".phone").mask("+7 (999) 99-99"); 
+   
+  
+   jQuery('.send-form').click( function() {
+	   var form = jQuery(this).closest('form');
+	   
+	   if ( form.valid() ) {
+		   form.css('opacity','.5');
+		   var actUrl = form.attr('action');
+
+		   jQuery.ajax({
+			   url: actUrl,
+			   type: 'post',
+			   dataType: 'html',
+			   data: form.serialize(),
+			   success: function(data) {
+				   form.html(data);
+				   form.css('opacity','1');
+				   //form.find('.status').html('форма отправлена успешно');
+				   //$('#myModal').modal('show') // для бутстрапа
+			   },
+			   error:	 function() {
+					form.find('.status').html('серверная ошибка');
+			   }
+		   });
+	   }
+   });
 	
 	/* scroll */
 	
@@ -35,20 +65,6 @@ $(document).ready(function() {
 		return "<span>" + s[0] + "</span><span>" + s[1] + "</span>";
 	}
 	update();
-
-	/* sliders */
-
-	$(".owl-carousel").owlCarousel({
-		items: 1,
-		loop: true,
-		autoHeight: true,
-		smartSpeed: 300,
-		mouseDrag: false,
-		pullDrag: true,
-		nav: true,
-		navText: ""
-	});
-
 });
 
 let lastScrollTop = 0;
@@ -83,5 +99,4 @@ menuItems.forEach(function(item, idx) {
     item.addEventListener('click', function(e) {
         menu.classList.remove('show-menu');
     })
-})
-
+});
